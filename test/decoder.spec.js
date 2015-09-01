0c3b4be23f07f624c5a379081f29fdfd60d04b77
Maik Riechert <maik.riechert@arcor.de>
2015-09-01 17:10:58 +0100

    prepare tests and create .npmignore

---
+++ test/decoder.spec.js
@@ -0,0 +1,11 @@
+var assert = require('assert')
+var decoder = require('/decoder')
+
+describe('decoder', function() {
+  it('should perform endianness conversion', function() {
+    // TODO
+  })
+  it('should choose the correct array type', function() {
+    // TODO    
+  })
+})

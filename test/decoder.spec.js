77e1dd3cad2d9aac2691ce7fa6c87904699a29df
Maik Riechert <maik.riechert@arcor.de>
2015-09-02 12:27:59 +0100

    added endianness test

--- test/decoder.spec.js
+++ test/decoder.spec.js
@@ -1,9 +1,34 @@
 var assert = require('assert')
-var decoder = require('../decoder.js')
+var TypedArrayDecoder = require('../decoder.js')
+
+function isPlatformLittleEndian () {
+  var buffer = new ArrayBuffer(2)
+  new DataView(buffer).setInt16(0, 256, true)
+  return new Int16Array(buffer)[0] === 256
+}
 
 describe('decoder', function() {
   it('should perform endianness conversion', function() {
-    // TODO
+    var data = [14, 1, 2888, 4543222]
+    var typedArr = new Uint32Array(data.length)
+    for (var i=0; i < data.length; i++) {
+      typedArr[i] = data[i]
+    }
+    
+    // change endianness
+    var little = isPlatformLittleEndian()
+    var dv = new DataView(typedArr.buffer)
+    for (var i=0; i < typedArr.length; i++) {
+      dv.setUint32(i*4, typedArr[i], !little)
+    }
+    
+    // check if the decoder can read it
+    var arr = new Uint8Array(typedArr.buffer)
+    var tag = little ? 66 : 70
+    var result = TypedArrayDecoder.decode(arr, tag)
+    var resultArr = Array.prototype.slice.call(result)
+    
+    assert.deepEqual(result, data)
   })
   it('should choose the correct array type', function() {
     // TODO    
